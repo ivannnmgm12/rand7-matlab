f746177a6ce61257437be06c64485db2e8f0ff36
ivannnmgm12 <99719931+ivannnmgm12@users.noreply.github.com>
2026-01-15 12:35:25 +0100

    Add files via upload

---
+++ rand7.m
@@ -0,0 +1,29 @@
+
+% devuelve un entero aleatorio entre 1 y 7 usando rand5()
+% funcion rand5() ya dada
+
+function r = rand7() 
+
+ while true 
+    a = rand5();     % devuelve un entero aleatorio entre 1 y 5
+    b = rand5();
+    
+    num = (a - 1) * 5 + b;  % numero entre 1 y 25
+
+    if num <= 21
+        r = mod(num - 1, 7) + 1;  % numero entre 1 y 7 unif.
+        return;
+       
+    end
+
+ end
+end
+
+
+
+function x = rand5()
+    x = randi(5);
+end
+
+
+
